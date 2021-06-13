3acb38c9898a88112267a10025abb4ace2487430
Umberto Loria SAM <umberto.loria@gmail.com>
2021-06-13 10:10:18 +0200

    Introduced single film retrival, improved pagination, changed port to 8080

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const mongo = require('./dbconn');
+const ObjectId = require('mongodb').ObjectID;
 
 const express = require('express');
-const {query, validationResult} = require('express-validator');
+const {query, param, validationResult} = require('express-validator');
+const cors = require('cors');
+
 const app = express();
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors())
 
 mongo().then(({films}) => {
 
 	// Tutti i film paginati con filtri titolo-anno-genere, ordinati per titolo-anno-genere-durata (de/)crescenti
 	app.route('/film').get(
-		query('titleQuery').isString().optional(),
-		query('year').isInt().toInt().optional(),
-		query('genre').isString().optional(),
-		query('sort').matches(/(title|year|genre|duration) (asc|desc)/i).toLowerCase().optional(),
+		query('titolo').isString().optional(),
+		query('anno').isInt().toInt().optional(),
+		query('genere').isString().optional(),
+		query('sort').matches(/(titolo_italiano|anno|genere|durata|voto_medio) (asc|desc)/i).toLowerCase().optional(),
 		query('pageNum').isInt({min: 1}).toInt().optional(),
 		query('pageSize').isInt({min: 1}).toInt().optional(),
 		async (req, res) => {
@@ -24,12 +25,12 @@ mongo().then(({films}) => {
 			}
 
 			// Filters
-			const criteria = {
+			let criteria = {
 				$and: [],
 			};
-			let {titleQuery, year, genre} = req.query;
-			if (titleQuery) {
-				const uniqueTitleWords = titleQuery.trim().replace(/ +/g, ' ').split(' ')
+			let {titolo, anno, genere} = req.query;
+			if (titolo) {
+				const uniqueTitleWords = titolo.trim().replace(/ +/g, ' ').split(' ')
 					.filter((item, index, arr) => arr.indexOf(item) === index);
 				const reg = new RegExp(uniqueTitleWords.join('|'), 'i');
 				criteria.$and.push({
@@ -39,32 +40,35 @@ mongo().then(({films}) => {
 					],
 				});
 			}
-			if (year) {
+			if (anno) {
 				criteria.$and.push({
-					anno: {$eq: year},
+					anno: {$eq: anno},
 				});
 			}
-			if (genre) {
+			if (genere) {
 				criteria.$and.push({
-					genere: {$eq: genre},
+					genere: {$eq: genere},
 				});
+			}
+			if (criteria.$and.length === 0) {
+				criteria = {};
 			}
 
 			// Sorting
 			let sort;
-			if (req.query.sort) {
-				const [field, orderStr] = req.query.sort.split(' ');
-				const orderInt = orderStr === 'asc' ? 1 : -1;
-				const sortingObjects = {
-					'title': {titolo_italiano: orderInt},
-					'year': {anno: orderInt},
-					'genre': {genere: orderInt},
-					'duration': {durata: orderInt},
-				};
-				sort = sortingObjects[field]
-			} else {
-				sort = {titolo_italiano: 1};
+			if (!req.query.sort) {
+				req.query.sort = 'titolo_italiano asc';
 			}
+			const [field, orderStr] = req.query.sort.split(' ');
+			const orderInt = orderStr === 'asc' ? 1 : -1;
+			const sortingObjects = {
+				'titolo_italiano': {titolo_italiano: orderInt, _id: 1},
+				'anno': {anno: orderInt, _id: 1},
+				'genere': {genere: orderInt, _id: 1},
+				'durata': {durata: orderInt, _id: 1},
+				'voto_medio': {voto_medio: orderInt, _id: 1},
+			};
+			sort = sortingObjects[field]
 
 			// Pagination
 			const pagination = {
@@ -72,60 +76,113 @@ mongo().then(({films}) => {
 				pageSize: req.query.pageSize || 10,
 			};
 
-			const paginatedFilms = await films.find(criteria).sort(sort)
-				.limit(pagination.pageSize).skip((pagination.pageNum - 1) * pagination.pageSize)
-				.toArray().catch(e => {
+			const numSkip = (pagination.pageNum - 1) * pagination.pageSize;
+			const paginatedFilms = await films
+				.find(criteria)
+				.sort(sort)
+				.limit(pagination.pageSize)
+				.skip(numSkip)
+				.toArray()
+				.catch(e => {
 					console.error(e);
-				})
+				});
 			const totalFilms = await films.countDocuments(criteria);
-			console.log('Paginated films ' + paginatedFilms.length + ' over ' + totalFilms);
-			const result = {paginatedFilms, totalFilms};
+			console.log('Paginated ' + paginatedFilms.length + ' films over ' + totalFilms + ' skipping ' + numSkip);
+			const result = {
+				paginatedFilms,
+				totalFilms,
+				pageNum: pagination.pageNum,
+				pageSize: pagination.pageSize,
+				sortBy: field,
+				descending: orderInt < 0,
+			};
 			return res.json(result);
+		});
+
+	// Il film corrispondente a un dato ID
+	app.route('/film/:id').get(
+		param('id').isString(),
+		async (req, res) => {
+			const errors = validationResult(req);
+			if (!errors.isEmpty()) {
+				return res.status(400).json({errors: errors.array()});
+			}
+			const id = req.params.id;
+			console.log('Looking for film ' + id);
+			try {
+				const retrievedFilm = await films.findOne({_id: {$eq: new ObjectId(id)}});
+				if (retrievedFilm) {
+					return res.json(retrievedFilm);
+				}
+				return res.sendStatus(404);
+			} catch (err) {
+				console.error(err);
+				return res.sendStatus(400);
+			}
 		});
 
 	// Tutti i film usciti in un dato anno con un dato attore
 	app.route('/film/:year([0-9]+)/:actor').get(
 		async (req, res) => {
 			const {year, actor} = req.params;
-			const matchingFilms = await films.find({
-				anno: {$eq: parseInt(year)},
-				attori: {
-					$in: [actor],
-				},
-			}).toArray().catch(e => {
-				console.error(e);
-			})
+			const matchingFilms = await films
+				.find({
+					anno: {$eq: parseInt(year)},
+					attori: {
+						$in: [actor],
+					},
+				})
+				.toArray()
+				.catch(e => {
+					console.error(e);
+				})
 			return res.json(matchingFilms);
 		});
 
 	// Tutti i film girati da un dato regista, ordinati per anno di uscita crescente
-	app.route('/film/:director').get(
+	app.route('/film/regista/:director').get(
+		param('director').isString().isLength({min: 1}),
 		async (req, res) => {
+			const errors = validationResult(req);
+			if (!errors.isEmpty()) {
+				return res.status(400).json({errors: errors.array()});
+			}
 			const {director} = req.params;
-			const matchingFilms = await films.find({
-				registi: {
-					$in: [director],
-				},
-			}).sort({anno: 1}).toArray().catch(e => {
-				console.error(e);
-			})
+			const matchingFilms = await films
+				.find({
+					registi: {
+						$in: [director],
+					},
+				})
+				.sort({anno: 1})
+				.toArray()
+				.catch(e => {
+					console.error(e);
+				})
 			return res.json(matchingFilms);
 		});
 
 	// Tutti i film di un dato genere votati da un dato numero minimo, ordinati per numero di voti decrescente
-	app.route('/film/:genre/:minimum([0-9]+)').get(
+	app.route('/film/genere/:genere/voti-minimi/:minimo([0-9]+)').get(
+		param('director').isString().isLength({min: 1}),
+		param('genere').isString().isLength({min: 1}),
+		param('minimo').isInt({min: 0, max: 10}),
 		async (req, res) => {
-			const {genre, minimum} = req.params;
-			const matchingFilms = await films.find({
-				genere: {$eq: genre},
-				voti: {$gte: parseInt(minimum)},
-			}).sort({voti: -1}).toArray().catch(e => {
-				console.error(e);
-			})
+			const {genere, minimo} = req.params;
+			const matchingFilms = await films
+				.find({
+					genere: {$eq: genere},
+					voti: {$gte: parseInt(minimo)},
+				})
+				.sort({voti: -1})
+				.toArray()
+				.catch(e => {
+					console.error(e);
+				})
 			return res.json(matchingFilms);
 		});
 
 })
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 app.listen(port);
